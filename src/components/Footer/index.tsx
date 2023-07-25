c35221e15c783162a94362f68a1613c82271fcc8
AllianceTing <2426446427@qq.com>
2023-07-26 00:21:00 +0800

    feat: modify design
    Signed-off-by: AllianceTing <2426446427@qq.com>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { DefaultFooter } from '@ant-design/pro-components';
 import '@umijs/max';
 import React from 'react';
 const Footer: React.FC = () => {
-  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const defaultMessage = 'ZooGroup体验技术部出品';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
@@ -13,20 +13,20 @@ const Footer: React.FC = () => {
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
+          key: 'Zoo Design Group',
+          title: 'Zoo Design Group',
           href: 'https://pro.ant.design',
           blankTarget: true,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/Klee-cocow/Zoo-AIGC-Front.git',
           blankTarget: true,
         },
         {
-          key: 'Ant Design',
-          title: 'Ant Design',
+          key: 'Zoo Design',
+          title: 'Zoo Design',
           href: 'https://ant.design',
           blankTarget: true,
         },
